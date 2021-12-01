09a70f7eaed8f2ab3f3b8e917d9fa6692201ae5e
mdk44 <matt.karam@gmail.com>
2021-12-01 15:37:51 -0600

    Part 1 done

--- Day_01/Day_01.py
+++ Day_01/Day_01.py
@@ -9,14 +9,16 @@ for line in lines:
     nums.append(int(line))
 
 # Part 1
-for x in range(0, len(nums)):
-    for y in range(x + 1, len(nums)):
-        if nums[x] + nums[y] == 2020:
-            print("Part 1: " + str(nums[x] * nums[y]))
+p1_count = 0
+for x in range(1, len(nums)):
+    if nums[x] > nums[x - 1]:
+        p1_count += 1
+
+print(p1_count)
 
 # Part 2
-for x in range(0, len(nums)):
-    for y in range(x + 1, len(nums)):
-        for z in range(y + 1, len(nums)):
-            if nums[x] + nums[y] + nums[z] == 2020:
-                print("Part 2: " + str(nums[x] * nums[y] * nums[z]))
+# for x in range(0, len(nums)):
+#     for y in range(x + 1, len(nums)):
+#         for z in range(y + 1, len(nums)):
+#             if nums[x] + nums[y] + nums[z] == 2020:
+#                 print("Part 2: " + str(nums[x] * nums[y] * nums[z]))
