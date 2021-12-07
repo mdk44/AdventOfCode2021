6f69f5223bb1fdd252fad0826c92e8945633e3ec
mdk44 <matt.karam@gmail.com>
2021-12-06 18:06:33 -0600

    Day 5 Part 1 complete

--- Day_05/Day_05.py
+++ Day_05/Day_05.py
@@ -1,7 +1,7 @@
 import re
 
-# input_file = 'Day_05\\Input.csv'
-input_file = 'Day_05\\Test.csv'
+input_file = 'Day_05\\Input.csv'
+# input_file = 'Day_05\\Test.csv'
 text_file = open(input_file)
 lines = text_file.read().split('\n')
 
@@ -24,6 +24,7 @@ for line in lines:
     if int(y2) > max_y:
         max_y = int(y2)
 
+# Part 1
 grid = {}
 for y in range(0, max_y + 1):
     grid[y] = {}
@@ -31,12 +32,34 @@ for y in range(0, max_y + 1):
         grid[y][x] = 0
 
 for num in num_list:
-    for y in range(num[1], num[3]):
-        for x in range(num[0], num[2]):
-            grid[y][x] += 1
-
+    x1 = min(num[0], num[2])
+    x2 = max(num[0], num[2])
+    y1 = min(num[1], num[3])
+    y2 = max(num[1], num[3])
+    if y1 == y2:
+        for x in range(x1, x2 + 1):
+            grid[y1][x] += 1
+    elif x1 == x2:
+        for y in range(y1, y2 + 1):
+            grid[y][x1] += 1
+        
+mult_ints = 0
 for y in grid:
     print_line = ''
     for x in grid[y]:
-        print_line += str(grid[y][x])
-    print(print_line)
+        if grid[y][x] > 1:
+            mult_ints += 1
+        if grid[y][x] == 0:
+            print_line += '.'
+        else:
+            print_line += str(grid[y][x])
+    # print(print_line)
+
+print("Part 1: " + str(mult_ints))
+
+# Part 2
+grid = {}
+for y in range(0, max_y + 1):
+    grid[y] = {}
+    for x in range(0, max_x + 1):
+        grid[y][x] = 0
