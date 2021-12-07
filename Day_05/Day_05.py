3df97062497e2be414979189fcd20fe76b6ec6a2
mdk44 <matt.karam@gmail.com>
2021-12-06 19:35:33 -0600

    Using functions for day 5

--- Day_05/Day_05.py
+++ Day_05/Day_05.py
@@ -5,6 +5,7 @@ input_file = 'Day_05\\Test.csv'
 text_file = open(input_file)
 lines = text_file.read().split('\n')
 
+# Define the grid size based off of input
 num_list = []
 max_x = 0
 max_y = 0
@@ -24,45 +25,50 @@ for line in lines:
     if int(y2) > max_y:
         max_y = int(y2)
 
+def build_grid():
+    grid = {}
+    for y in range(0, max_y + 1):
+        grid[y] = {}
+        for x in range(0, max_x + 1):
+            grid[y][x] = 0
+    return grid
+
+def straight_lines(inp, grid):
+    result = 0
+    for num in inp:
+        x1 = min(num[0], num[2])
+        x2 = max(num[0], num[2])
+        y1 = min(num[1], num[3])
+        y2 = max(num[1], num[3])
+        if y1 == y2:
+            for x in range(x1, x2 + 1):
+                grid[y1][x] += 1
+        elif x1 == x2:
+            for y in range(y1, y2 + 1):
+                grid[y][x1] += 1
+    for y in grid:
+        for x in grid[y]:
+            if grid[y][x] > 1:
+                result += 1
+    return grid, result
+
+def print_grid(grid):
+    for y in grid:
+        print_line = ''
+        for x in grid[y]:
+            if grid[y][x] == 0:
+                print_line += '.'
+            else:
+                print_line += str(grid[y][x])
+        print(print_line)
+
 # Part 1
-grid = {}
-for y in range(0, max_y + 1):
-    grid[y] = {}
-    for x in range(0, max_x + 1):
-        grid[y][x] = 0
-
-for num in num_list:
-    x1 = min(num[0], num[2])
-    x2 = max(num[0], num[2])
-    y1 = min(num[1], num[3])
-    y2 = max(num[1], num[3])
-    if y1 == y2:
-        for x in range(x1, x2 + 1):
-            grid[y1][x] += 1
-    elif x1 == x2:
-        for y in range(y1, y2 + 1):
-            grid[y][x1] += 1
-        
-mult_ints = 0
-for y in grid:
-    print_line = ''
-    for x in grid[y]:
-        if grid[y][x] > 1:
-            mult_ints += 1
-        if grid[y][x] == 0:
-            print_line += '.'
-        else:
-            print_line += str(grid[y][x])
-    # print(print_line)
-
-print("Part 1: " + str(mult_ints))
+grid = build_grid()
+grid, result = straight_lines(num_list, grid)
+print("Part 1: " + str(result))
 
 # Part 2
-grid = {}
-for y in range(0, max_y + 1):
-    grid[y] = {}
-    for x in range(0, max_x + 1):
-        grid[y][x] = 0
+grid = build_grid()
 
 for num in num_list:
     x1 = min(num[0], num[2])
