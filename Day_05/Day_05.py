b23e20df8a4c1397cf8a9b25b33671a6092feef4
mdk44 <matt.karam@gmail.com>
2021-12-06 20:15:29 -0600

    Trying functions

--- Day_05/Day_05.py
+++ Day_05/Day_05.py
@@ -34,6 +34,7 @@ def build_grid():
     return grid
 
 def straight_lines(inp, grid):
+    new_grid = grid.copy()
     result = 0
     for num in inp:
         x1 = min(num[0], num[2])
@@ -42,15 +43,53 @@ def straight_lines(inp, grid):
         y2 = max(num[1], num[3])
         if y1 == y2:
             for x in range(x1, x2 + 1):
-                grid[y1][x] += 1
+                new_grid[y1][x] += 1
         elif x1 == x2:
             for y in range(y1, y2 + 1):
-                grid[y][x1] += 1
+                new_grid[y][x1] += 1
     for y in grid:
-        for x in grid[y]:
-            if grid[y][x] > 1:
+        for x in new_grid[y]:
+            if new_grid[y][x] > 1:
                 result += 1
-    return grid, result
+    return new_grid, result
+
+def diagonals(inp, grid):
+    new_grid = grid.copy()
+    result = 0
+    for num in inp:
+        x1 = num[0]
+        x2 = num[2]
+        y1 = num[1]
+        y2 = num[2]
+    if abs(x1 - x2) / abs(y1 - y2) == 1:
+        i = x1
+        j = y1
+        if x1 > x2 and y1 > y2:
+            while i >= x2:
+                new_grid[j][i] += 1
+                i -= 1
+                j -= 1
+        elif x1 < x2 and y1 > y2:
+            while i <= x2:
+                new_grid[j][i] += 1
+                i += 1
+                j -= 1
+        elif x1 > x2 and y1 < y2:
+            while i >= x2:
+                new_grid[j][i] += 1
+                i -= 1
+                j += 1
+        elif x1 < x2 and y1 < y2:
+            while i <= x2:
+                new_grid[j][i] += 1
+                i += 1
+                j += 1
+    for y in new_grid:
+        for x in new_grid[y]:
+            if new_grid[y][x] > 1:
+                result += 1
+    return new_grid, result
+    
 
 def print_grid(grid):
     for y in grid:
@@ -69,52 +108,7 @@ print("Part 1: " + str(result))
 
 # Part 2
 grid = build_grid()
-
-for num in num_list:
-    x1 = min(num[0], num[2])
-    x2 = max(num[0], num[2])
-    y1 = min(num[1], num[3])
-    y2 = max(num[1], num[3])
-    if y1 == y2:
-        for x in range(x1, x2 + 1):
-            grid[y1][x] += 1
-    elif x1 == x2:
-        for y in range(y1, y2 + 1):
-            grid[y][x1] += 1
-    elif abs(x2 - x1) / abs(y2 - y1) == 1:
-        i = num[0]
-        j = num[1]
-        if num[1] > num[3] and num[0] > num[2]:
-            while i >= num[2]:
-                grid[j][i] += 1
-                i -= 1
-                j -= 1
-        elif num[1] > num[3] and num[0] < num[2]:
-            while i <= num[2] and j <= num[3]:
-                grid[j][i] += 1
-                i += 1
-                j -= 1
-        elif num[1] < num[3] and num[0] > num[2]:
-            while i >= num[2] and j <= num[3]:
-                grid[j][i] += 1
-                i -= 1
-                j += 1
-        elif num[1] < num[3] and num[0] < num[2]:
-            while i <= num[2] and j <= num[3]:
-                grid[j][i] += 1
-                i += 1
-                j += 1
-
-mult_ints = 0
-for y in grid:
-    print_line = ''
-    for x in grid[y]:
-        if grid[y][x] > 1:
-            mult_ints += 1
-        if grid[y][x] == 0:
-            print_line += '.'
-        else:
-            print_line += str(grid[y][x])
-    print(print_line)
-
-print("Part 2: " + str(mult_ints))
+grid, result = straight_lines(num_list, grid)
+grid, result = diagonals(num_list, grid) # everything up to THIS POINT is working peachy, so need to reconfigure diagonal function
+print("Part 2: " + str(result))
+print_grid(grid)
