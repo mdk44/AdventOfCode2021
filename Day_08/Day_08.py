5ea823aa8178f9e4b3fb28f4977a0109f676dac5
mdk44 <matt.karam@gmail.com>
2021-12-08 09:23:53 -0600

    Day 8 tinkering, no real progress

--- Day_08/Day_08.py
+++ Day_08/Day_08.py
@@ -17,6 +17,52 @@ digits = {
     "9": 'abcdfg'
 }
 
+letters = {
+    "a": '',
+    "b": '',
+    "c": '',
+    "d": '',
+    "e": '',
+    "f": '',
+    "g": ''
+}
+
+def read_input(line):
+    inputs = []
+    code = []
+    inps = line.split('|')[0].split(' ')
+    outps = line.split('|')[1].split(' ')
+    for inp in inps:
+        if inp != '':
+            inputs.append(inp)
+    for outp in outps:
+        if outp != '':
+            code.append(outp)
+    return inputs, code
+
+def convert_letters(line):
+    new_letters = {}
+    new_letters = letters.copy()
+    inputs, code = read_input(line)
+    for inp in inputs:
+        if len(inp) == 7:
+            new_letters["a"]= inp[0]
+            new_letters["b"]= inp[1]
+            new_letters["c"]= inp[2]
+            new_letters["d"]= inp[3]
+            new_letters["e"]= inp[4]
+            new_letters["f"]= inp[5]
+            new_letters["g"]= inp[6]
+    print(new_letters)
+    print(inputs)
+
+
 new_digits = {}
 new_digits = digits.copy()
+# key_list = list(new_digits.keys())
+# val_list = list(new_digits.values())
+# pos = val_list.index('cf')
+# print(key_list[pos])
 
+for line in lines:
+    convert_letters(line)
