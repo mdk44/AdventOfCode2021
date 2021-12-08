0f67180dc97ad3926d6abe1a88ab8ae8da41439b
mdk44 <matt.karam@gmail.com>
2021-12-08 13:44:20 -0600

    Day 8 complete and f you

--- Day_08/Day_08.py
+++ Day_08/Day_08.py
@@ -1,5 +1,5 @@
-# input_file = 'Day_08\\Input.csv'
-input_file = 'Day_08\\Test.csv'
+input_file = 'Day_08\\Input.csv'
+# input_file = 'Day_08\\Test.csv'
 # input_file = 'Day_08\\Test2.csv'
 text_file = open(input_file)
 lines = text_file.read().split('\n')
@@ -17,16 +17,6 @@ digits = {
     "9": 'abcdfg'
 }
 
-letters = {
-    "a": '',
-    "b": '',
-    "c": '',
-    "d": '',
-    "e": '',
-    "f": '',
-    "g": ''
-}
-
 def read_input(line):
     inputs = []
     code = []
@@ -40,22 +30,6 @@ def read_input(line):
             code.append(outp)
     return inputs, code
 
-def convert_letters(line):
-    new_letters = {}
-    new_letters = letters.copy()
-    inputs, code = read_input(line)
-    for inp in inputs:
-        if len(inp) == 7:
-            new_letters["a"]= inp[0]
-            new_letters["b"]= inp[1]
-            new_letters["c"]= inp[2]
-            new_letters["d"]= inp[3]
-            new_letters["e"]= inp[4]
-            new_letters["f"]= inp[5]
-            new_letters["g"]= inp[6]
-    print(new_letters)
-    print(inputs)
-
 def part_1(line):
     result = 0
     inputs, code = read_input(line)
@@ -64,12 +38,107 @@ def part_1(line):
             result += 1
     return result
 
-new_digits = {}
-new_digits = digits.copy()
-# key_list = list(new_digits.keys())
-# val_list = list(new_digits.values())
-# pos = val_list.index('cf')
-# print(key_list[pos])
+def new_letters(line, letters):
+    inputs, code = read_input(line)
+    inputs.sort(key = len)
+    all_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    seen_letters = []
+    for inp in inputs:
+        if len(inp) == 2:
+            for i in inp:
+                letters["c"].append(i)
+                letters["f"].append(i)
+                if i not in seen_letters:
+                    seen_letters.append(i)
+        if len(inp) == 3:
+            for i in inp:
+                if i not in seen_letters:
+                    letters["a"].append(i)
+                    seen_letters.append(i)
+        if len(inp) == 4:
+            for i in inp:
+                if i not in seen_letters:
+                    letters["b"].append(i)
+                    letters["d"].append(i)
+                    seen_letters.append(i)
+        if len(inp) == 6:
+            count = 0
+            for i in inp:
+                if i in seen_letters:
+                    count += 1
+            if count == 5 and len(seen_letters) == 5:
+                for i in inp:
+                    if i not in seen_letters:
+                        letters["g"].append(i)
+                        seen_letters.append(i)
+                letters["e"].append(list(set(all_letters) - set(seen_letters))[0])
+    for inp in inputs:
+        if len(inp) == 6 and len(letters["c"]) > 1:
+            hunt = letters["c"]
+            for h in range(0, 2):
+                if hunt[h] not in inp:
+                    letters["c"] = [hunt[h]]
+                    if h == 0:
+                        letters["f"] = [hunt[1]]
+                    else:
+                        letters["f"] = [hunt[0]]
+    for inp in inputs:
+        if len(inp) == 6 and len(letters["b"]) > 1:
+            hunt = letters["b"]
+            for h in range(0, 2):
+                if hunt[h] not in inp:
+                    letters["d"] = [hunt[h]]
+                    if h == 0:
+                        letters["b"] = [hunt[1]]
+                    else:
+                        letters["b"] = [hunt[0]]
+
+def reset_key():
+    lets = {
+        "a": [],
+        "b": [],
+        "c": [],
+        "d": [],
+        "e": [],
+        "f": [],
+        "g": [] 
+    }
+    return lets
+            
+def replace_letters(line):
+    key = reset_key().copy()
+    new_digits = {}
+    new_digits = digits.copy()
+    new_code = []
+    new_letters(line, key)
+    key_list = list(key.keys())
+    val_list = list(key.values())
+    input, code = read_input(line)
+    for cd in code:
+        conv = ''
+        for c in cd:
+            pos = val_list.index([c])
+            conv += key_list[pos]
+        new_code.append(''.join(sorted(conv)))
+    return new_code
+
+def find_digits(line):
+    code = replace_letters(line)
+    key_list = list(digits.keys())
+    val_list = list(digits.values())
+    result = ''
+    for cd in code:
+        digit = ''
+        pos = val_list.index(cd)
+        digit += key_list[pos]
+        result += digit
+    return int(result)
+
+def run_sum(lines):
+    cum_sum = 0
+    for line in lines:
+        cum_sum += find_digits(line)
+    return cum_sum
 
 # Part 1
 cum_sum = 0
@@ -77,3 +146,4 @@ for line in lines:
     cum_sum += part_1(line)
 
 print("Part 1: " + str(cum_sum))
+print("Part 2: " + str(run_sum(lines)))
