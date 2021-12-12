f84c85ce79e8ef1076f5d9f21c8d1968b687108b
mdk44 <matt.karam@gmail.com>
2021-12-11 21:14:39 -0600

    Day 11 complete - holy moly

--- Day_11/Day_11.py
+++ Day_11/Day_11.py
@@ -1,5 +1,5 @@
-# input_file = 'Day_11\\Input.csv'
-input_file = 'Day_11\\Test.csv'
+input_file = 'Day_11\\Input.csv'
+# input_file = 'Day_11\\Test.csv'
 text_file = open(input_file)
 lines = text_file.read().split('\n')
 
@@ -26,38 +26,44 @@ def increase_energy(grid):
     for y in range(0, max_y):
         for x in range(0, max_x):
             grid[y][x] += 1
+        
+def adjacent_points(y, x):
+    return [(y + j, x + i) for j in range(-1, 2) for i in range(-1, 2) if (j, i) != (0, 0) and 0 <= y + j <= 9 and 0 <= x + i <= 9]
 
 def flash_point(grid, y, x):
-    if y == 0:
-        check_y = [0, 1]
-    elif y == max_y - 1:
-        check_y = [-1, 0]
-    else:
-        check_y = [-1, 0, 1]
-    if x == 0:
-        check_x = [0, 1]
-    elif x == max_x - 1:
-        check_x = [-1, 0]
-    else:
-        check_x = [-1, 0, 1]
-    if grid[y][x] > 9:
-        grid[y][x] == 0
-        for j in range(0, len(check_y)):
-            for i in range(0, len(check_x)):
-                new_y = check_y[j]
-                new_x = check_x[i]
-                if grid[y + new_y][x + new_x] != 0:
-                    grid[y + new_y][x + new_x] += 1
+    grid[y][x] = 0
+    for j, i in adjacent_points(y, x):
+        if grid[j][i] != 0:
+            grid[j][i] += 1
+        if grid[j][i] > 9:
+            grid[j][i] = 0
+            flash_point(grid, j, i)
 
 def flash_grid(grid):
+    flashes = 0
+    increase_energy(grid)
     for y in range(0, max_y):
         for x in range(0, max_x):
             if grid[y][x] > 9:
                 flash_point(grid, y, x)
 
+def find_zeros(grid):
+    flashes = 0
+    for y in range(0, max_y):
+        for x in range(0, max_x):
+            if grid[y][x] == 0:
+                flashes += 1
+    return flashes
+
 grid = build_grid(lines)
-increase_energy(grid)
-flash_grid(grid)
-increase_energy(grid)
-flash_grid(grid)
+
+flashes = 0
+for i in range(1, 1000):
+    flash_grid(grid)
+    flashes += find_zeros(grid)
+    if i == 100:
+        print("Part 1: " + str(flashes))
+    if find_zeros(grid) == 100:
+        print("Part 2: " + str(i))
+        break
 
