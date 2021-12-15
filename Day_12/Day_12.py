83acb4f7e62311e1d7aeb2964f84c146edc81550
mdk44 <matt.karam@gmail.com>
2021-12-15 12:09:58 -0600

    Finished Day 12, set up Day 15

--- Day_12/Day_12.py
+++ Day_12/Day_12.py
@@ -1,15 +1,9 @@
-# input_file = 'Day_12\\Input.csv'
-input_file = 'Day_12\\Test.csv'
+input_file = 'Day_12\\Input.csv'
+# input_file = 'Day_12\\Test.csv'
 text_file = open(input_file)
 lines = text_file.read().split('\n')
 
-# Here's how I would do it:
-# create a loop that builds multiple lists with all possibilities, then delete each one that either doesn't hit the end or has to cross a lowercase twice
-# build a list of lowercase strings and remove them as you cross them
-# Experiment with an __init__ and self function setup
-
 connections = {}
-lowercases = []
 for line in lines:
     p1 = line.split('-')[0]
     p2 = line.split('-')[1]
@@ -21,20 +15,26 @@ for line in lines:
         connections[p1].append(p2)
     if p1 not in connections[p2]:
         connections[p2].append(p1)
-    if p1.islower() and p1 not in lowercases:
-        lowercases.append(p1)
-    if p2.islower() and p2 not in lowercases:
-        lowercases.append(p2)
 
-def generate_path(lowers):
-    l = lowers[:]
-    paths = []
-    for p in connections['start']:
-        paths.append(connections[p])
-    l.remove('start')
-    print(paths)
-    print(l)
+del connections['end']
+for c in connections:
+    if 'start' in connections[c]:
+        connections[c].remove('start')
 
-generate_path(lowercases)
-print(lowercases)
+print(connections)
 
+def generate_path(path = ['start']):
+    paths = 0
+    for p in connections[path[-1]]:
+        if p.isupper() or p not in path:
+            paths += 1 if p == 'end' else generate_path(path + [p])
+    return paths
+
+def generate_p2(path = ['start']):
+    paths = 0
+    for p in connections[path[-1]]:
+        paths += 1 if p == 'end' else (generate_p2, generate_path)[p.islower() and p in path](path + [p])
+    return paths
+
+print("Part 1: " + str(generate_path()))
+print("Part 2: " + str(generate_p2()))
---
+++ Day_15/Day_15.py
@@ -0,0 +1,6 @@
+# input_file = 'Day_15\\Input.csv'
+input_file = 'Day_15\\Test.csv'
+text_file = open(input_file)
+lines = text_file.read().split('\n')
+
+# Another damn path exercise
